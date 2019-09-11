cebeef06085a3ec23aa53039ae5c9c844cb66a5f
Cao <elina.cao@sap.com>
2019-09-11 16:02:42 +0800

    1

---
+++ bingSearch.py
@@ -0,0 +1,21 @@
+import json
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+API_KEY = "1d747ded252e468a8fff93c3fda96a71"
+BASE_URL = "https://api.cognitive.microsoft.com/bing/v7.0/localbusinesses/search"
+
+@app.route("/", methods=["GET", "POST"])
+def helloWorld():
+	return render_template("index.html")
+@app.route("/ping", methods=["GET", "POST"])
+def ping():
+	headers = {'Ocp-Apim-Subscription-Key': API_KEY}
+	params = {'q': request.args.get("q"), 'mkt': 'en-us'}
+	ret = requests.get(url=BASE_URL, headers=headers, params=params)
+	return json.dumps(ret.json(), indent=4)
+
+if(__name__ == "__main__"):
+	app.run(host="0.0.0.0", port="8888", debug=True)
