f2a06d7ac1307e957e78098b60f05e8e7f966b0b
Cao <elina.cao@sap.com>
2019-09-19 18:56:05 +0800

    1

--- bingSearch.py
+++ bingSearch.py
@@ -9,13 +9,29 @@ BASE_URL = "https://api.cognitive.microsoft.com/bing/v7.0/localbusinesses/search
 
 @app.route("/", methods=["GET", "POST"])
 def helloWorld():
-	return render_template("index.html")
+	return render_template("index1.html")
 @app.route("/ping", methods=["GET", "POST"])
 def ping():
 	headers = {'Ocp-Apim-Subscription-Key': API_KEY}
-	params = {'q': request.args.get("q"), 'mkt': 'en-us', 'localCircularView':'47.6421,-122.13715,5000'}
+	params = {'q': request.args.get("q"), 'mkt': 'en-us', 'localCircularView':'47.6421, -122.13715, 5000'}
 	ret = requests.get(url=BASE_URL, headers=headers, params=params)
-	return json.dumps(ret.json(), indent=4)
+	retList = json_to_obj(ret.text)
+	retDic = {}
+	for i in range(len(retList)):
+		retDic[i] = retList[i]
+	return json.dumps(retDic)
+
+def json_to_obj(dict):
+    my_dict = json.loads(dict)
+    obj_list,obj_dict =[],{}
+    values = my_dict['places']['value']
+    key_list = ['name','geo','routablePoint']
+    for value in values:
+        curName = value["name"]
+        curGeo = value["geo"]
+        curAddress = value["address"]
+        obj_list.append({'name':curName, 'geo':curGeo, 'address': curAddress})
+    return obj_list
 
 if(__name__ == "__main__"):
 	app.run(host="0.0.0.0", port="8888", debug=True)
