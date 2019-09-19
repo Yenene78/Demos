abdabb02f067089bb078163b5b5e96d0e3c8bd9c
Cao <elina.cao@sap.com>
2019-09-19 19:57:11 +0800

    1

--- bingSearch.py
+++ bingSearch.py
@@ -13,13 +13,28 @@ def helloWorld():
 @app.route("/ping", methods=["GET", "POST"])
 def ping():
 	headers = {'Ocp-Apim-Subscription-Key': API_KEY}
-	params = {'q': request.args.get("q"), 'mkt': 'en-us', 'localCircularView':'47.6421, -122.13715, 5000'}
+	params = {'q': qa_to_answer(request.args.get("q")), 'mkt': 'en-us', 'localCircularView':'47.6421, -122.13715, 5000'}
 	ret = requests.get(url=BASE_URL, headers=headers, params=params)
 	retList = json_to_obj(ret.text)
 	retDic = {}
 	for i in range(len(retList)):
 		retDic[i] = retList[i]
 	return json.dumps(retDic)
+
+def qa_to_answer(input_string):
+    url = "https://hackathonshqna.azurewebsites.net/qnamaker/knowledgebases/382f9a08-1901-4635-a524-33468a7cbad0/generateAnswer"
+    payload = "{\"question\":\"%s\"}"%input_string
+    headers = {
+    'content-type': "application/json",
+    'authorization': "EndpointKey 2360357e-0935-4082-b7c3-8ce9018e5b3c",
+    'cache-control': "no-cache",
+    'postman-token': "733fe038-4b97-cf5f-fcd1-b15431526553"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    s = response.text
+    my_dict = json.loads(s)
+    answer = my_dict['answers'][0]['answer']
+    return answer
 
 def json_to_obj(dict):
     my_dict = json.loads(dict)
